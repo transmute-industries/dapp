46bd3a780e68aa4a3893ddd3eef625a07746cdb2
Orie Steele <orie@ohs.io>
2017-08-03 20:13:04 -0500

    Comment out Uglify with shame

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -244,20 +244,28 @@ module.exports = {
     // It is absolutely essential that NODE_ENV was set to production here.
     // Otherwise React will be compiled in the very slow development mode.
     new webpack.DefinePlugin(env.stringified),
+
+
+
+    // SO BROKEN.... 
     // Minify the code.
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        // This feature has been reported as buggy a few times, such as:
-        // https://github.com/mishoo/UglifyJS2/issues/1964
-        // We'll wait with enabling it by default until it is more solid.
-        reduce_vars: false,
-      },
-      output: {
-        comments: false,
-      },
-      sourceMap: true,
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false,
+    //     // This feature has been reported as buggy a few times, such as:
+    //     // https://github.com/mishoo/UglifyJS2/issues/1964
+    //     // We'll wait with enabling it by default until it is more solid.
+    //     reduce_vars: false,
+    //   },
+    //   output: {
+    //     comments: false,
+    //   },
+    //   sourceMap: true,
+    // }),
+
+
+
+
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
       filename: cssFilename,
