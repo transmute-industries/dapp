c4a3cd757b597c0c3543ed961fb613164059e29f
Orie Steele <orie@ohs.io>
2017-08-03 20:34:21 -0500

    update for testnet build

--- src/transmute.ts
+++ src/transmute.ts
@@ -1,7 +1,14 @@
 import TransmuteFramework from 'transmute-framework';
 
 let config: any = {
-    env: 'testrpc',
+    env: 'infura',
+    ipfsConfig: {
+        host: 'ipfs.infura.io',
+        port: '5001',
+        options: {
+            protocol: 'https'
+        }
+    },
     aca: require('./contracts/RBAC'),
     esa: require('./contracts/UnsafeEventStore'),
     esfa: require('./contracts/UnsafeEventStoreFactory')
--- truffle.js
+++ truffle.js
@@ -1,11 +1,25 @@
 
 
+const LightWalletProvider = require('@digix/truffle-lightwallet-provider')
+
 module.exports = {
   networks: {
     development: {
       host: process.env.RPC_HOST || 'localhost',
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: new LightWalletProvider({
+        keystore: './sigmate-v3-ti.json',
+        password: process.env.PASSWORD,
+        rpcUrl: 'https://ropsten.infura.io',
+        debug: true, // optional, show JSON-RPC logs
+        // prefund: 1e18, // optional, fund all lightwallet addresses (via coinbase) with this of wei
+        pollingInterval: 2000 // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
+      }),
+      network_id: '*',
+
+    },
   }
 };
