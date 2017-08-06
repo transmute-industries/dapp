b748299f2d93ce3409a514f87e7148474d653f98
Orie Steele <orie@ohs.io>
2017-08-06 17:42:51 -0500

    prepare to retrieve results from query string

--- src/store/bioid/reducer.ts
+++ src/store/bioid/reducer.ts
@@ -1,5 +1,23 @@
 
+
+function getParameterByName(name: any, url: any) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 const handlers = {
+  ['@@router/LOCATION_CHANGE']: (state: any, action: any) => {
+    let bcid = getParameterByName('bcid', action.payload.search)
+    return {
+      ...state,
+      bcid: bcid
+    };
+  },
   ['BCID_RECEIVED']: (state: any, action: any) => {
     return {
       ...state,
